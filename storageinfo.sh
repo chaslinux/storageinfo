3b153db10c79b5bec4a2eafb04993a0f15f1ca80
chaslinux <chaslinux@cr-workstation1.theworkingcentre.org>
2024-05-17 08:47:46 -0400

    Simplified the script for just a hard drive.

--- storageinfo.sh
+++ storageinfo.sh
@@ -5,7 +5,7 @@
 # chaslinux@gmail.com
 # Licensed under GPL v3
 
-# The purpose of this script is to create a small information sheet about hard drives or nvme drives
+# The purpose of this script is to create a small information sheet about hard drives 
 # in order to provide information for someone who might buy a drive from our refurbishing project.
 # Our goal is to give buyers information about drives so they can make a better educated decision about
 # whether the drive will be acceptable for their use case.
@@ -22,46 +22,13 @@ sudo apt -y install texlive-extra-utils # allow cropping of pdf
 # Variable declarattions
 SDDRIVE=$(ls -1 /dev/sd?)
 SDFAMILY=$(sudo smartctl -d ata -a -i "$SDDRIVE" | grep "Model Family")
-SDSERIAL=$(sudo smartctl -a $SDDRIVE | grep "Serial Number:" | cut -c 20- )
-SDMODEL=$(sudo smartctl -a $SDDRIVE | grep "Model Number:" | cut -c 20- )
-SDSIZE=$(sudo smartctl -a $SDDRIVE | grep "Namespace 1 Size/Capacity:" | cut -c 28- )
-SDPOWERON=$(sudo smartctl -a $SDDRIVE | grep "Power On Hours:" | cut -c 20- )
-NVDRIVE=$(ls -1 /dev/nvme?)
-NVSERIAL=$(sudo smartctl -a $NVDRIVE | grep "Serial Number:" | cut -c 20- )
-NVMODEL=$(sudo smartctl -a $NVDRIVE | grep "Model Number:" | cut -c 20- )
-NVSIZE=$(sudo smartctl -a $NVDRIVE | grep "Namespace 1 Size/Capacity:" | cut -c 28- )
-NVPOWERON=$(sudo smartctl -a $NVDRIVE | grep "Power On Hours:" | cut -c 20- )
+SDSERIAL=$(sudo smartctl -a $SDDRIVE | grep "Serial Number" | cut -c 19- )
+SDMODEL=$(sudo smartctl -a $SDDRIVE | grep "Device Model" | cut -c 19- )
+SDSIZE=$(sudo smartctl -a $SDDRIVE | grep "User Capacity:" | cut -c 19- )
+SDPOWERON=$(sudo smartctl -a $SDDRIVE | grep "Power_On_Hours" | cut -c 88- )
 
-# if the storage.tex file exists on the user's desktop, tell them to delete it first
-if [ -f /home/"$USER"/Desktop/storageinfo.tex ]; then
-{
-	echo "storageinfo.tex exists, not writing anything, please delete the file from the desktop"
-	echo "and re-run the script."
-}
-fi
-
-# create the storage.tex file does not exist
-if [ ! -f /home/"$USER"/Desktop/storageinfo.tex ]; then
-{
-	# Hard Drive info
-	if sudo lshw -short | grep -m1 sda; then
-	{
-		echo -e "Model: \t\t"  $SDMODEL >> /home/"$USER"/Desktop/storageinfo.tex
-		echo -e "Serial No.: \t" $SDSERIAL >> /home/"$USER"/Desktop/storageinfo.tex
-		echo -e "Size: \t\t" $SDSIZE >> /home/"$USER"/Desktop/storageinfo.tex
-		echo -e "Hours: \t\t" $SDPOWERON >> /home/"$USER"/Desktop/storageinfo.tex
-	}
-	fi
-
-	# NVMe Drive info
-	if sudo lshw -short | grep -m1 nvme; then
-	{
-		echo -e "Model: \t\t"  $NVMODEL >> /home/"$USER"/Desktop/storageinfo.tex
-		echo -e "Serial No.: \t" $NVSERIAL >> /home/"$USER"/Desktop/storageinfo.tex
-		echo -e "Size: \t\t" $NVSIZE >> /home/"$USER"/Desktop/storageinfo.tex
-		echo -e "Hours: \t\t" $NVPOWERON >> /home/"$USER"/Desktop/storageinfo.tex
-	}
-	fi
-
-} 
-fi
+echo -e "Brand: \t\t"  $SDFAMILY >> /home/"$USER"/Desktop/storageinfo.tex
+echo -e "Model: \t\t" $SDMODEL >> /home/"$USER"/Desktop/storageinfo.tex
+echo -e "Serial No.: \t" $SDSERIAL >> /home/"$USER"/Desktop/storageinfo.tex
+echo -e "Size: \t\t" $SDSIZE >> /home/"$USER"/Desktop/storageinfo.tex
+echo -e "Hours On: \t\t" $SDPOWERON >> /home/"$USER"/Desktop/storageinfo.tex
